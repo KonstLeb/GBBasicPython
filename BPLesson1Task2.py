7f04b9519accd4bf7f4dac9a9cb8e13409a6484f
KonstLeb <74100477+KonstLeb@users.noreply.github.com>
2020-11-11 22:13:46 +0300

    Delete BPLesson1Task2.py

--- BPLesson1Task2.py
+++
@@ -1,6 +0,0 @@
-timesec = int (input ("Введите время в секундах: "))
-hours = timesec // 3600
-minutes = (timesec - hours*3600) // 60
-seconds = (timesec - hours*3600) % 60
-print (f"{hours:02d}:{minutes:02d}:{seconds:02d}")
-input()
