81037a5e681ee1f2d796e009a66b519d261556c0
KonstLeb <74100477+KonstLeb@users.noreply.github.com>
2020-11-11 22:14:04 +0300

    Delete BPLesson1Task4.py

--- BPLesson1Task4.py
+++
@@ -1,9 +0,0 @@
-number = input("Введите целое положительное число: ")
-maximum = number[0]
-k = 1
-while k < len(number):
-    if number[k] > maximum:
-        maximum = number[k]
-    k += 1
-print (f"Самая большая цифра в числе {number} - это {maximum}")
-input()
